83e8a9f5d3a241a843f4d76a8548b0324f3a8160
sunithvs <sunithvs2002@gmail.com>
2025-04-24 16:43:23 +0530

    fixing fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,30 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import { inter, monaSans } from "@/lib/fonts";
-import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+import localFont from 'next/font/local'
+
 import { RazorpayProvider } from "@/components/user/purchase/razorpay-provider";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+// Load Mona Sans as a local font
+const monaSans = localFont({
+  src: [
+    {
+      path: '../public/fonts/MonaSans-Italic-VariableFont_wdth,wght.ttf',
+      weight: '200 900', // Variable font weight range
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-mona-sans', // Define CSS variable
+})
 
 export const metadata: Metadata = {
   title: "Course Management",
--- lib/utils/file.ts
+++ lib/utils/file.ts
@@ -1,6 +1,9 @@
 export function getTimestampedFileName(originalName: string): string {
   const timestamp = Date.now();
-  const extension = originalName.split('.').pop();
-  const baseName = originalName.split('.').slice(0, -1).join('.');
+  // clean file name with space removal and lowercase
+    const cleanedName = originalName.replace(/\s+/g, '_').toLowerCase();
+
+  const extension = cleanedName.split('.').pop();
+  const baseName = cleanedName.split('.').slice(0, -1).join('.');
   return `${baseName}_${timestamp}.${extension}`;
 }
